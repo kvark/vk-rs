e7155fadff82de593ca258128edd47aaac464390
Osspial <osspial@gmail.com>
2016-04-27 16:19:15 -0400

    Updated load_with() to allow partial loading of Vulkan commands

--- vk_generator/src/generator/load_fn.rs
+++ vk_generator/src/generator/load_fn.rs
@@ -1,7 +1,6 @@
     fn_buf = load_fn({0}::RAW_NAME);
     if ptr::null() != fn_buf {{
         {0}::fn_ptr = mem::transmute(fn_buf); 
-    }} else {{
+    }} else if {0}::fn_ptr == unloaded_function_panic as *const _ {{
         unloaded_fns.push({0}::RAW_NAME);
     }}
-    
