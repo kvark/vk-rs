5b639c8cc978248ede208799117db7024aa6fb03
Osspial <osspial@gmail.com>
2016-04-27 20:31:14 -0400

    Fixed version macros

--- vk_generator/src/generator/defines.rs
+++ vk_generator/src/generator/defines.rs
@@ -1,19 +1,19 @@
 #[macro_export]
 macro_rules! vk_make_version {
-    ($major: expr, $minor: expr, $patch: expr) => (($major as uint32_t << 22) | ($minor as uint32_t << 12) | $patch as uint32_t)
+    ($major: expr, $minor: expr, $patch: expr) => ((($major as uint32_t) << 22) | (($minor as uint32_t) << 12) | $patch as uint32_t)
 }
 
 #[macro_export]
 macro_rules! vk_version_major {
-    ($major: expr) => ($major as uint32_t << 22)
+    ($major: expr) => (($major as uint32_t) << 22)
 }
 
 #[macro_export]
 macro_rules! vk_version_minor {
-    ($minor: expr) => (($minor as uint32_t << 12) & 0x3ff)
+    ($minor: expr) => ((($minor as uint32_t) << 12) & 0x3ff)
 }
 
 #[macro_export]
 macro_rules! vk_version_patch {
-    ($minor: expr) => ($minor as uint32_t & 0xfff)
+    ($minor: expr) => (($minor as uint32_t) & 0xfff)
 }
